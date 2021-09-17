d449c85334e599dfb8e8fcacb18031ea841351cd
Keith Gamble <keith.gamble@bwdesigngroup>
2021-09-17 14:31:17 -0700

    Added the Numeric Input and the Dropdown

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,12 +1,31 @@
 import time
-from perspective_automation.selenium import Session
+import platform
+from selenium.webdriver.common.by import By
+from perspective_automation.selenium import Session, TextBox, TextArea, NumericInput, Dropdown
+
 def test_session():
-    URL = "https://lv1mesdevlap01.est1933.com:8043/data/perspective/client/MES"
+    URL = "http://localhost/data/perspective/client/MES/component-test-fixture"
     CREDENTIALS = {"username":"RATester01", "password":"N3verp@tch2021"}
     session = Session(URL, 10, CREDENTIALS)
     session.login()
+
+    viewPath = TextBox(session, By.ID, "viewPathTextField")
+    viewPath.setText("Operations/Calculators/Tub")
+
+    viewParams = TextArea(session, By.ID, "viewParamsTextBox")
+    viewParams.setText({"WRK_ORD_LEG_N": 5,"WRK_ORD_SYS_I": 1958622,"stat": "IP" })
+    
+    dropdown = Dropdown(session, By.ID, "tubCalculatorDropdown")
+    dropdown.setValue("Tub30")
+
+    inchesToAdd = NumericInput(session, By.ID, "inchesToAdd")
+    inchesToAdd.setValue(123.45)
+
     time.sleep(10)
 
 
 if __name__ == "__main__":
    test_session()
+
+
+
