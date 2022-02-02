fc19aa0e0c485ae5d56f9201a3d75fa37be855d6
Joel <Joel.Boersma@ejgallo.com>
2022-02-02 10:52:49 -0800

    PerspectiveElement: use different super().__init__ args based on selenium version

--- src/perspective_automation/perspective.py
+++ src/perspective_automation/perspective.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium import __version__ as SELENIUM_VERSION
 
 
 class ElementNotFoundException(Exception):
@@ -45,8 +46,12 @@ class PerspectiveElement(WebElement):
             else:
                 element = session.waitForElement(identifier, locator, timeout_in_seconds=timeout_in_seconds)
 
-        # I am not sure why w3c has to be true or this _.find_element_by_xxx fails?
-        super().__init__(element.parent, element.id, w3c=True)
+        SELENIUM_MAJOR_VERSION = int(str(SELENIUM_VERSION)[0])
+        if SELENIUM_MAJOR_VERSION <= 3:
+            # I am not sure why w3c has to be true or this _.find_element_by_xxx fails?
+            super().__init__(element.parent, element.id, w3c=True)
+        else:
+            super().__init__(element.parent, element.id)
 
     def find_element_by_partial_class_name(self, name) -> WebElement:
         return super().find_element_by_xpath(".//*[contains(@class, '%s')]" % name)
