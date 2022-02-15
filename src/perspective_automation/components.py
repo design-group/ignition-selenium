b7389394e7aa70c7ecf7c627ca38d69afe874d36
Bernadette Davoud <Bernadette.Davoud@ejgallo.com>
2022-02-15 13:58:52 -0800

    getHeaders modified to return TableCells

--- src/perspective_automation/components.py
+++ src/perspective_automation/components.py
@@ -726,7 +726,7 @@ class Table(PerspectiveComponent):
 
     def getHeaders(self) -> list[TableCell]:
         headerElements = self.waitForElements(By.CLASS_NAME, self.header_cell_class_name)
-        return [TableCell(self.session, element=element).getDataId() for element in headerElements]
+        return [TableCell(self.session, element=element) for element in headerElements]
 
     def getRowCount(self) -> int:
         num_pages = self.getNumPages()
